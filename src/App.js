fb4b7186949bc1dad05e1fa6ee8e65a67fc1b40b
Anastasiya Mitrofanova <mitrofanovaa70@gmail.com>
2024-03-12 17:30:43 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,75 @@
+function App() {
+  return (
+    <div className="wrapper">
+      <header className="header">
+        <div className="header__left">
+          <img />
+          <div className="header__info"> 
+            <h3 className="header__title">Mitrofan Shop</h3>
+            <p className="header__name">Лучший магазин</p>
+          </div>
+        </div>
+        <ul className="header__right">
+          <li>
+            <img />
+            <span>1205 p.</span>
+          </li>
+          <li>
+            <img />
+          </li>
+        </ul>
+      </header>
+      <main className="content">
+        <h1 className="content__title">Все товары</h1>
+        <section className="content__cards">
+          <div className="card">
+            <img className="card__img"></img>
+            <h3 className="card__title">Желтые кроссы</h3>
+            <div className="card__buy">
+              <div>
+                <p>Цена :</p>
+                <b>1200 p.</b>
+              </div>
+              <button>Купить</button>
+            </div>
+          </div>
+          <div className="card">
+            <img className="card__img"></img>
+            <h3 className="card__title">Желтые кроссы</h3>
+            <div className="card__buy">
+              <div>
+                <p>Цена :</p>
+                <b>1200 p.</b>
+              </div>
+              <button>Купить</button>
+            </div>
+          </div>
+          <div className="card">
+            <img className="card__img"></img>
+            <h3 className="card__title">Желтые кроссы</h3>
+            <div className="card__buy">
+              <div>
+                <p>Цена :</p>
+                <b>1200 p.</b>
+              </div>
+              <button>Купить</button>
+            </div>
+          </div>
+          <div className="card">
+            <img className="card__img"></img>
+            <h3 className="card__title">Желтые кроссы</h3>
+            <div className="card__buy">
+              <div>
+                <p>Цена :</p>
+                <b>1200 p.</b>
+              </div>
+              <button>Купить</button>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
