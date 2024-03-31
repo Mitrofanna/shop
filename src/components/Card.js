ee5213377fca0bf487d61f69ab64b250e8476d42
Anastasiya Mitrofanova <mitrofanovaa70@gmail.com>
2024-04-01 00:12:22 +0300

    выравнивает карточки

--- src/components/Card.js
+++ src/components/Card.js
@@ -20,12 +20,13 @@ function Card({imageUrl, title, price, onClickAddCart, onClickFavorite}) {
       <div className="card__buy">
         <div className="card__price-wrapper">
           <p className="card__price">Цена :</p>
-          <b>{price}</b>
+          <b>{price} ₽</b>
         </div>
         <div className="card__button-favorite" onClick={onClickFavoriteBtn}>
           <svg className={isFavorite && 'card__favorite-add'} aria-hidden="true" focusable="false" fill="#c3c3c3" width="17" height="18" viewbox="0 0 17 18" xmlns="http://www.w3.org/2000/svg">
             <path d="M3.993 2.185l.017-.092V2c0-.554.449-1 .99-1h10c.522 0 .957.41.997.923l-2.736 14.59-4.814-2.407-.39-.195-.408.153L1.31 16.44 3.993 2.185z"></path>
           </svg>
+          <span className="visually-hidden">Добавить в избранное.</span>
         </div>
         <button className={`card__button ${!isAdded ? 'card__button-default' : 'card__button-active'}`} onClick={onClickAddButton}>
           <span className="visually-hidden">Добавить в корзину.</span>
