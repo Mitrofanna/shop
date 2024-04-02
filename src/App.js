95053f2525032ae557998f744d7509e29a8aa680
Anastasiya Mitrofanova <mitrofanovaa70@gmail.com>
2024-04-02 17:31:09 +0300

    маршрутизация

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import axios from "axios";
 import Header from "./components/Header";
-import Card from "./components/Card";
 import Drawer from "./components/Drawer";
-import Promo from "./components/Promo";
 import Footer from "./components/Footer";
-import Empty from "./components/Empty";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
 
 function App() {
   const [items, setItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
   const [cartOpened, setCartOpened] = useState(false);
   const [searchItems, setSearchItems] = useState('');
-  const [favorites, setIsFavorites] = useState([]);
+  const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     axios.get('https://660317272393662c31ce874c.mockapi.io/items').then((res) => {
-      setItems(res.data)
-    })
+      setItems(res.data);
+    });
     axios.get('https://660317272393662c31ce874c.mockapi.io/cart').then((res) => {
-      setCartItems(res.data)
-    })
+      setCartItems(res.data);
+    });
+    axios.get('https://660add09ccda4cbc75dbf3f3.mockapi.io/favorites').then((res) => {
+      setFavorites(res.data);
+    });
   }, []);
 
   const onAddItem = (obj) => {
@@ -30,7 +33,7 @@ function App() {
 
   const onAddFavorites = (obj) => {
     axios.post('https://660add09ccda4cbc75dbf3f3.mockapi.io/favorites', obj); 
-    setIsFavorites(prev => [...prev, obj]);
+    setFavorites(prev => [...prev, obj]);
   };
 
   const onRemoveItem = (id) => {
@@ -42,39 +45,25 @@ function App() {
     setSearchItems(event.target.value);
   };
 
-  const itemsSorted = items.filter((item) => item.title.toLowerCase().includes(searchItems.toLowerCase()));
-
   return (
     <div className="wrapper">
       {cartOpened && <Drawer items={cartItems} onCloseCart={() => setCartOpened(false)} onRemove={onRemoveItem}/>}
-        <Header onClickCart={() => setCartOpened(true)} items={favorites} />
-      <main className="content">
-        <Promo />
-        <div className="content__wrapper">
-          <h1 className="content__title">{searchItems ? `Поиск по запросу: ${searchItems}` : `Все товары`} </h1>
-          <div className="search">
-            <input id="search" className="search__input" type="text" value={searchItems} onChange={onSearchItems} />
-            <label id="search" className="search__label">Поиск...</label>
-            <svg className="search__img" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 18 18" fill="#f2f2f2"><path d="m17.7 16.06-3.48-3.48a7.63 7.63 0 0 0 1.53-4.7c0-4.4-3.48-7.88-7.88-7.88A7.81 7.81 0 0 0 0 7.88c0 4.4 3.48 7.87 7.88 7.87 1.73 0 3.37-.61 4.7-1.53l3.48 3.47c.2.2.4.31.82.31.6 0 1.12-.51 1.12-1.13 0-.2-.1-.5-.3-.81ZM2.24 7.88a5.67 5.67 0 0 1 5.63-5.63 5.67 5.67 0 0 1 5.62 5.63 5.67 5.67 0 0 1-5.63 5.62 5.67 5.67 0 0 1-5.62-5.63Z"/></svg>
-          </div>
-        </div>
-        <section className="content__cards">
-         {itemsSorted.length > 0 ? 
-          itemsSorted.map((item) => (
-          <Card 
-            key={item.id}
-            id={item.id}
-            title={item.title}
-            price={item.price}
-            imageUrl={item.imageUrl}
-            isFavorite={item.isFavorite}
-            isAdded={item.isAdded}
-            onClickAddCart={(obj) => onAddItem(obj)}
-            onClickFavorite={(obj) => onAddFavorites(obj)}
-          />
-          )) : <Empty />}
-        </section>
-      </main>
+      <Header onClickCart={() => setCartOpened(true)} items={favorites} />
+        <Routes>
+          <Route path="/" element={<Home
+            items={items}
+            searchItems={searchItems}
+            onAddItem={onAddItem}
+            onAddFavorites={onAddFavorites}
+            onSearchItems={onSearchItems}
+          />}>
+        </Route>
+        <Route path="/favorites" element={<Favorites
+          items={favorites} 
+        />}>
+
+        </Route>
+      </Routes>
       <Footer />
     </div>
   );
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,9 @@
+function Favorites() {
+    return (
+        <main className="favorites">
+            <h1>Избранные товары</h1>
+        </main>
+    );
+}
+
+export default Favorites;
