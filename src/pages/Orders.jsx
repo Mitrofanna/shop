3ea22b0308f792d134a21c6c9a0d2a511032b95b
Anastasiya Mitrofanova <mitrofanovaa70@gmail.com>
2024-04-06 13:11:07 +0300

    стили для страницы заказов

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -2,6 +2,8 @@ function Orders() {
     return (
         <main className="content">
             <h1 className="content__title">Покупки</h1>
+            <img className="content__img-order1" width="180" height="190" src="./img/content/orders-background1.png" alt="Фото наших скейтов."></img>
+            <img className="content__img-order2" width="180" height="190" src="./img/content/orders-background2.png" alt="Фото наших скейтов."></img>
         </main>
     );
 }
