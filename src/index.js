230bb20fb7c94fc11480fc6e71c10f866b94428c
Anastasiya Mitrofanova <mitrofanovaa70@gmail.com>
2024-04-10 23:07:22 +0300

    test

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/shop'>
       <App />
     </Router>
   </React.StrictMode>
